a3a8669dd1d510f83cda5ff37215ff9461d4a3e5
fanboimsft <fanboimsft@icloud.com>
2026-09-18 20:59:09 +0700

    fix cmake cross file

--- internal/hokuto/cmake_cross_test.go
+++ internal/hokuto/cmake_cross_test.go
@@ -25,6 +25,7 @@ func TestWriteCMakeToolchainFile(t *testing.T) {
 		"set(CMAKE_SYSTEM_NAME Linux)",
 		"set(CMAKE_SYSTEM_PROCESSOR aarch64)",
 		"set(CMAKE_C_COMPILER   aarch64-linux-gnu-gcc)",
+		"set(CMAKE_SYSROOT /usr/aarch64-linux-gnu)",
 		"set(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu)",
 		"set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)",
 		"set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)",
